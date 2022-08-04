48cec7e8f96cc688645728b7417c60367fc46643
twnkm7089 <twnkm7089@gmail.com>
2022-08-04 20:12:26 +0900

    add remove

--- js/add_del.js
+++ js/add_del.js
@@ -1,8 +1,9 @@
 function newList() {
+    //list 추가
     var newItem = document.createElement("li");
     var plan = document.querySelector("#quest");
     var newPlan = document.createTextNode(plan.value);
-    console.log(newPlan)
+  
     if(plan.value == "") {
         alert("내용을 입력해 주세요.");
     } else {
@@ -10,7 +11,21 @@ function newList() {
 
         var planList = document.querySelector("#planList");
         planList.insertBefore(newItem, planList.childNodes[0]);
+
     }
 
     plan.value="";
+
+
+
+
+    //삭제
+    var items = document.querySelectorAll("li");
+    for(i = 0; i < items.length; i++) {
+        items[i].addEventListener("click", function() {
+            if(this.parentNode)
+                this.parentNode.removeChild(this);
+        })
+    }
 }
+
